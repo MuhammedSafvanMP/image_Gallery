33a475034d7cc7d87691438c62956de7d92aa060
Muhammed Safvan MP <muhammedsafvanmp7@gmail.com>
2023-10-07 15:18:59 +0530

    video palay & pause js added

--- js/video.js
+++ js/video.js
@@ -1,8 +1,8 @@
 const videosWrapper = document.querySelector(".videos");
 const loadMoreBtn = document.querySelector(".load-more");
-const searchInput = document.querySelector(".inpu-box input");
+const searchInput = document.querySelector(".search-box input");
 
-const apiKEY = "YOUR_PEXELS_API_KEY"; // Replace with your Pexels API key
+const apiKEY = "VP29sp0n1JS84DgIhIwO35HLaMjxqWN9KAbNK8ByhU1hZOzgcIkD9ux3"; // Replace with your Pexels API key
 const perPage = 15;
 let currentPage = 1;
 let searchTerm = null;
@@ -10,7 +10,7 @@ let searchTerm = null;
 const generateHTML = (videos) => {
     return videos.map((vid) => {
         return `<li class="card">
-        <video autoplay loop src="${vid.video_files[0].link}"></video>
+        <video class="play" loop muted src="${vid.video_files[0].link}"></video>
         <div class="details d-flex align-content-center justify-content-between position-absolute">
             <div class="photographer d-flex align-items-center">
                 <i class="fa-solid fa-camera"></i>
@@ -28,15 +28,35 @@ const getVideos = (apiURL) => {
     fetch(apiURL, {
         headers: { Authorization: apiKEY },
     })
-        .then((res) => res.json())
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error(`API request failed with status ${res.status}`);
+            }
+            return res.json();
+        })
         .then((data) => {
             console.log(data);
             const generatedHTML = generateHTML(data.videos);
             videosWrapper.innerHTML += generatedHTML;
             loadMoreBtn.innerText = "Load More";
             loadMoreBtn.classList.remove("disabled");
+
+            // Add event listeners to play videos on hover and pause on mouse leave
+            const videoElements = videosWrapper.querySelectorAll("video.play");
+            videoElements.forEach((video) => {
+                video.addEventListener("mouseenter", () => {
+                    video.play();
+                });
+                video.addEventListener("mouseleave", () => {
+                    video.pause();
+                });
+            });
         })
-        .catch(() => alert("Failed to load videos!"));
+        .catch((error) => {
+            console.error("Error loading videos:", error);
+            loadMoreBtn.innerText = "Load More";
+            loadMoreBtn.classList.remove("disabled");
+        });
 };
 
 const loadMoreVideos = () => {
@@ -48,18 +68,16 @@ const loadMoreVideos = () => {
     getVideos(apiURL);
 };
 
-const loadSearchVideos = (e) => {
-    if (e.target.value === "") return (searchTerm = null);
-    if (e.key === "Enter") {
-        currentPage = 1;
-        searchTerm = e.target.value;
-        videosWrapper.innerHTML = "";
-        let apiURL = `https://api.pexels.com/videos/search?query=${searchTerm}&page=${currentPage}&per_page=${perPage}`;
-        getVideos(apiURL);
-    }
+const loadSearchVideos = () => {
+    // Update the searchTerm in real-time
+    searchTerm = searchInput.value.trim();
+    currentPage = 1;
+    videosWrapper.innerHTML = "";
+    let apiURL = `https://api.pexels.com/videos/search?query=${searchTerm}&page=${currentPage}&per_page=${perPage}`;
+    getVideos(apiURL);
 };
 
 getVideos(`https://api.pexels.com/videos/popular?page=${currentPage}&per_page=${perPage}`);
 
 loadMoreBtn.addEventListener("click", loadMoreVideos);
-searchInput.addEventListener("keyup", loadSearchVideos);
+searchInput.addEventListener("input", loadSearchVideos);
